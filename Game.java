83eb6386f8c99b8e4d2075844f65714302c46efb
Karcz Przemysław <pkarc@PGI.LOCAL>
2021-10-19 15:07:23 +0200

    files removed from TicTacToe draft repo to new repo

---
+++ Game.java
@@ -0,0 +1,23 @@
+package pl.com.sda.homework.Wlasne_Projekty.TicTacToe;
+
+public class Game{
+
+    public String[][] makesMoveInGame(String horizontally, String vertically, String circle_or_cross){
+
+        Prints prints = new Prints();
+        TwoDimensionalTable twoDimensionalTable = new TwoDimensionalTable();
+
+        int parseIntHorizontally = Integer.parseInt(horizontally);
+        int parseIntVertically = Integer.parseInt(vertically);
+
+        String[][] returnedTable2D = twoDimensionalTable.getTable2D();
+
+        returnedTable2D[parseIntHorizontally - 1][parseIntVertically - 1] = circle_or_cross;
+
+        prints.printsTable2D();
+
+        return returnedTable2D;
+    }
+
+
+}
